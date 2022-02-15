7ce7a193c96c1849b8ebdf083bb8abf64356ffea
Lentezz <yaroslav.golovan@gmail.com>
2022-02-15 23:10:43 +0200

    commit

--- app/src/main/java/com/univer/transport_guide/MainActivity.java
+++ app/src/main/java/com/univer/transport_guide/MainActivity.java
@@ -1,5 +1,6 @@
 package com.univer.transport_guide;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -7,11 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
+import com.google.firebase.auth.AuthResult;
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -39,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         //registerLayout = findViewById(R.id.register_layout);
         btnRegister.setOnClickListener(view -> showRegisterWindow());
+        btnSignIn.setOnClickListener(view -> showSignInWindow());
     }
 
     public void showRegisterWindow(){
@@ -76,13 +83,47 @@ public class MainActivity extends AppCompatActivity {
                         user.setName(name.getText().toString());
                         user.setPassword(password.getText().toString());
 
-                        users.child(user.getEmail()).setValue(user).addOnSuccessListener(unused ->
+                        users.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user).addOnSuccessListener(unused ->
                                 Snackbar.make(registerLayout, "MMM Nice", Snackbar.LENGTH_LONG).show());
 
                     });
         });
         dialog.show();
 
+    }
+
+    public void showSignInWindow(){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Sign now");
+        dialog.setMessage("Write all now");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View signInWindow = inflater.inflate(R.layout.sign_in_window, null);
+
+
+        dialog.setView(signInWindow);
+
+
+        final MaterialEditText password = signInWindow.findViewById(R.id.passField);
+        final MaterialEditText email = signInWindow.findViewById(R.id.emailField);
+
+        dialog.setNegativeButton("Cancel", (dialogInterface, i) -> dialogInterface.cancel());
+        dialog.setPositiveButton("Sign In", (dialogInterface, i) -> {
+            if(TextUtils.isEmpty(email.getText().toString())){
+                Snackbar.make(registerLayout, "ERROR email", Snackbar.LENGTH_LONG).show();
+                return;
+            }
+            if(password.getText().toString().length() < 6){
+                Snackbar.make(registerLayout, "ERROR password", Snackbar.LENGTH_LONG).show();
+                return;
+            }
+            auth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                    .addOnSuccessListener(authResult -> {
+                        startActivity(new Intent(MainActivity.this, MapActivity.class));
+                        finish();
+                    }).addOnFailureListener(e -> Snackbar.make(registerLayout, "Error: " + e.getMessage(), BaseTransientBottomBar.LENGTH_SHORT).show());
+        });
+        dialog.show();
     }
 
 }
